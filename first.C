675c9cf680afbb65869b515dc14e49029f570db7
malak-err <erraoudymalak1997@gmail.com>
2024-11-21 09:42:59 +0100

    STRUCTURES

--- first.C
+++ first.C
@@ -1,23 +1,64 @@
-#include<stdio.h>
-typedef struct {
-    char nom[50];
-    int age;
-   Adresse adresse;
-
-}personne;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct {
     char rue[50];
     char ville[50];
-    char codepostale[50];
-    
+    int codepostale;
 } Adresse;
 
-personne pers;
+typedef struct {
+    char nom[50];
+    int age;
+    Adresse adresse;
+} Personne;
+
+Personne pers;
+
 void creerpersonne(){
-   printf("entrer le nom:");
-scanf ("%s",pers.nom);
-printf ("%d",&pers.age); 
+    printf("Entrer le nom : ");
+    scanf("%s", pers.nom);
+    printf("Entrer l'âge : ");
+    scanf("%d", &pers.age);
+    printf("Entrer la rue : ");
+    scanf("%s", pers.adresse.rue);
+    printf("Entrer la ville : ");
+    scanf("%s", pers.adresse.ville);
+    printf("Entrer le code postal : ");
+    scanf("%d", &pers.adresse.codepostale);
 }
 
+void afficherlespersonnes(Personne pers){
+    printf("Nom : %s\n", pers.nom);
+    printf("Âge : %d\n", pers.age);
+    printf("Rue : %s\n", pers.adresse.rue);
+    printf("Ville : %s\n", pers.adresse.ville);
+    printf("Le code postal : %d\n", pers.adresse.codepostale);
+}
 
+int main (){
+    int choix;
+    do {
+        printf("1 : créer une personne\n");
+        printf("2 : afficher la personne\n");
+        printf("3 : quitter\n");
+        printf("Entrer votre choix : ");
+        scanf("%d", &choix);
+        switch(choix){
+            case 1:
+                creerpersonne();
+                break;
+            case 2:
+                afficherlespersonnes(pers);
+                break;
+            case 3:
+                printf("Au revoir\n");
+                break;
+            default:
+                printf("Choix invalide\n");
+                break;
+        }
+    } while (choix != 3);
+    return 0;
+}
